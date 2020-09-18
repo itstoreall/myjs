c7b20ce6ca4a40a01770c060c92e2444268ed50e
Serhii Stanislav <serhiistanislav@gmail.com>
2020-09-18 20:57:47 +0300

    adds call stack, stack trace, arguments, rest, push

--- js/002.js
+++ js/002.js
@@ -514,6 +514,8 @@
  *
  * add(5, 3) - 5, 3 - аргумены - передаем при вызове функции и присваеваются в параметры
  *
+ * Одна функция делает только одну вещь
+ *
  */
 
 /*
@@ -638,5 +640,343 @@ fnA();
 // console.log("Лог перед вызовом функции C");
 // fnC();
 // console.log("Лог после вызова функции C");
+
+*/
+
+/*
+ * Задача
+ *
+ * Напиши функцию calvulatedTotalPrice(item)
+ * которая принимает массив цен (чисел) и возвращает их сумму
+ *
+ * Делаем код (функцию) который можно переиспользовать
+ *
+ */
+
+/*
+
+// const cart = [54, 28, 105, 78, 92, 17, 120, 12, 25, 98];
+// let total = 0;
+
+// for (const value of cart) {
+//   total += value;
+// }
+
+// console.log("Total: ", total);
+
+*/
+/*
+
+const calculateTotalPrice = function (items) {
+  console.log("items инутри функции: ", items); // выводит аргементы переданные дри вызове функции
+
+  let total = 0; // локальная переменная для хранения значения до возврата
+
+  for (const item of items) {
+    total += item;
+  }
+
+  return total; // возвращает значение во внешний код и выходит
+};
+
+const r1 = calculateTotalPrice([1, 2, 3]);
+const r2 = calculateTotalPrice([5, 10, 15, 20]);
+const r3 = calculateTotalPrice([100, 200, 300]);
+
+console.log(`Общая сумма покупок: ${r1}`); // 6
+console.log(`Общая сумма покупок: ${r2}`); // 50
+console.log(`Общая сумма покупок: ${r3}`); // 600
+
+*/
+
+/*
+ * Напиши функцию logItems(items) для перебора и логирования массива
+ * Делаем функцию без возврата, просто не указываем return
+ *
+ */
+
+/*
+
+const logItems = function (items) {
+  for (const item of items) {
+    console.log(item);
+  }
+};
+
+logItems(["Mango", "Kiwi", "Poly", "Ajax"]);
+logItems([1, 2, 3, 4, 5]);
+logItems(["клавиатура", "наушники", "часы"]);
+
+*/
+
+/*
+ * Наптши функцию для поиска логина
+ * - Если логина нет, вывести сообщение "Пользователь [логин] ненайден"
+ * - Если нашли логин, вывести сообщение, "Пользователь [логин] найден"
+ */
+
+/*
+
+const logins = ["m4ngoDoge", "k1widab3st", "poly1scute", "aj4xth3m4n"];
+
+// const message = logins.includes(loginToFind)
+//   ? `Пользователь ${loginToFind} найден`
+//   : `Пользователь ${loginToFind} не найден`;
+
+// console.log(message);
+
+const findLogin = function (allLogins, loginToFind) {
+  for (const login of allLogins) {
+    if (login === loginToFind) {
+      return `Пользователь ${loginToFind} найден`;
+    }
+  }
+
+  return `Пользователь ${loginToFind} не найден`;
+};
+
+console.log(findLogin(logins, "avocod3r"));
+console.log(findLogin(logins, "k1widab3st"));
+console.log(findLogin(logins, "jam4l"));
+console.log(findLogin(logins, "poly1scute"));
+
+*/
+
+/*
+ * Тоже самое но с тернарным оператором
+ */
+
+/*
+
+const logins = ["m4ngoDoge", "k1widab3st", "poly1scute", "aj4xth3m4n"];
+
+// const message = logins.includes(loginToFind)
+//   ? `Пользователь ${loginToFind} найден`
+//   : `Пользователь ${loginToFind} не найден`;
+
+// console.log(message);
+
+const findLogin = function (allLogins, loginToFind) {
+  return allLogins.includes(loginToFind)
+    ? `Пользователь ${loginToFind} найден`
+    : `Пользователь ${loginToFind} не найден`;
+};
+
+console.log(findLogin(logins, "avocod3r"));
+console.log(findLogin(logins, "k1widab3st"));
+console.log(findLogin(logins, "jam4l"));
+console.log(findLogin(logins, "poly1scute"));
+
+*/
+
+/*
+ * Напиши функцию findSmallesNumber(numbers) для поиска самого маленького числа в массиве
+ * при условии что числа уникальные (не повторяются)
+ *
+ */
+
+/*
+
+const numbers = [51, 18, 13, 24, 7, 85, 19];
+let smallestNumber = numbers[0];
+
+const findSmallesNumber = function (numbers) {
+  let smallestNumber = numbers[0];
+
+  for (const number of numbers) {
+    if (number < smallestNumber) {
+      smallestNumber = number;
+    }
+  }
+
+  return smallestNumber;
+};
+
+console.log(findSmallesNumber([3, 8, 12, -2, 15])); // -2
+console.log(findSmallesNumber([100, 54, 8, 12, 47])); // 8
+console.log(findSmallesNumber([7, 21, 84, 15, 4])); // 4
+
+*/
+
+/*
+ * Напиши функцию changeCase(string) которая заменяет регистр каждого символа в строке на противоположный
+ * Например если строка "JavaScript"
+ *
+ */
+
+/*
+
+const changeCase = function (string) {
+  const letters = string.split(""); // разбили letters побуквенно
+  let invertedString = "";
+
+  for (const letter of letters) {
+    const isInLowerCase = letter === letter.toLowerCase();
+
+    invertedString += isInLowerCase
+      ? letter.toUpperCase()
+      : letter.toLowerCase();
+  }
+
+  return invertedString;
+};
+
+console.log(changeCase("qweRTY"));
+console.log(changeCase("mAnGo"));
+console.log(changeCase("AjAx"));
+
+*/
+
+/*
+ * Напишите функцию slugify(string) которая получает строку и возвращает URL-slag
+ * Строка состоит только из букв и пробелов
+ *
+ */
+
+/*
+
+// const title = "Top 10 benefits of React framework";
+
+// const slug1 = title.toLowerCase().split(" ").join("-");
+
+// console.log(slug1);
+
+const slugify = function (string) {
+  return string.toLowerCase().split(" ").join("-"); // весь код функции в одной строке - абстракция
+
+  // const normalizedString = string.toLowerCase();
+  // // console.log(normalizedString); // новмализированная строка - все в нижний регистр
+
+  // const words = normalizedString.split(" ");
+  // // console.log(words); // не сшитый массив слов
+
+  // const slug = words.join("-");
+  // // console.log(slug); // готовый слаг - то что нужго вернуть
+
+  // return slug;
+};
+
+console.log(slugify("Top 10 benefits of React framework"));
+console.log(slugify("Azure Static Web Apps are Awesome"));
+
+*/
+
+/*
+ * Псевдомассив arguments и Array.from и ...
+ *
+ * * arguments - классический Old School
+ * * arguments - встроенная локальная переменная в функциях (кроме стрелочных)
+ * * arguments - это псевдомассив, у него нету всех метадов массива
+ */
+
+/*
+
+const fn = function () {
+  console.log(arguments);
+
+  const args = Array.from(arguments);
+
+  console.log(args);
+
+  // for (const arg of arguments) {
+  //   console.log(arg);
+  // }
+};
+
+fn(1, 2, 3);
+fn(1, 2, 3, 4, 5);
+fn(1, 2, 3, 4, 5, 6, 7);
+
+*/
+// /*
+
+/* -//- Современный метод
+ ** rest - специальная операция - остаток
+ ** rest - в скобках функции ставим (...имя функции)
+ ** в параметр (...имя) плпадают все элементы массива
+ */
+
+/*
+
+const fn = function (...args) {
+  console.log(args); // это настоящий массив из аргументов которые к нам пришли
+};
+
+fn(1, 2, 3);
+fn(1, 2, 3, 4, 5);
+fn(1, 2, 3, 4, 5, 6, 7);
+
+*/
+/*
+
+// - Вариант, когда нужно перед рестом обявить произвольное кол-во других параметров
+// - Логируем параметры поотдельности - ${a} ${b} ${c} и args
+
+const fn = function (a, b, c, ...args) {
+  console.log(`${a} ${b} ${c}`);
+  console.log(args);
+};
+
+fn("hello", 1, 2, 3);
+fn("aloha", 1, 2, 3, 4, 5);
+fn("hi", 1, 2, 3, 4, 5, 6, 7);
+
+*/
+
+/*
+ * Напиши функцию add для сложения произвольного количества аргументов (числа)
+ * - Операция ... (rest)
+ * Функция которая принимает произволльное кол-во чисел как аргументов
+ и возвращает их общую сумму
+ */
+
+/*
+
+const add = function (...args) {
+  console.log(args);
+
+  let total = 0;
+
+  for (const arg of args) {
+    total += arg;
+  }
+
+  return total;
+};
+
+console.log(add(1, 2, 3));
+console.log(add(1, 2, 4, 5, 6));
+
+*/
+
+/*
+ * Напиши функцию filterNumbers(array [, number1, ...]) которая:
+ * - первым аргументом принимает массив чисел
+ * - после первого аргумента может быть произвольное кол-во других аргументов которые будут числами
+ * - Функцция должна вернуть новый массив, в котором будут только те аргументы, начиная со второго, для которых есть аналог в оригинальном массиве
+ */
+
+/*
+
+const filterNumbers = function (array, ...args) {
+  console.log("array: ", array);
+  console.log("args: ", args);
+  const uniqueElements = [];
+
+  for (const element of array) {
+    console.log(args.includes(element)); // проверяем на наличие element в args
+
+    if (args.includes(element)) {
+      uniqueElements.push(element); // push - пушим в новый массмв uniqueElements
+      console.log(`${element} есть везде!`);
+    }
+  }
+
+  return uniqueElements;
+};
+
+console.log(filterNumbers([1, 2, 3, 4, 5], 10, 15, 2, 3, 8)); // [2, 3]
+console.log(filterNumbers([10, 15, 25, 30], 23, 30, 18, 15)); // [30, 15]
+console.log(filterNumbers([100, 200, 300, 400, 500], 7, 12, 200, 64)); // [200]
 
 */
