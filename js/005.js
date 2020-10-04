cc890e1d9f0dcf6456493c2e6b81f5bcc9c38c2f
Serhii Stanislav <serhiistanislav@gmail.com>
2020-10-04 20:48:38 +0300

    adds <code class="theme-code">, hasOwnProperty, class, static, get, set

--- js/005.js
+++ js/005.js
@@ -174,3 +174,233 @@ document.querySelector(".js-add-method").addEventListener("click", function () {
 console.log(Manager.prototype);
 
 */
+/*
+
+// Собственные свойства объекта и for...in
+
+const objA = {
+  q: 5,
+  z: 6,
+};
+
+// прототипом objB будет objA (в свойстве __proto__ ссылка на objA)
+// свойства которые находятся в objB называятся собственными
+// свойства в objB называются унаследованные
+const objB = Object.create(objA);
+
+objB.x = 1;
+objB.y = 2;
+
+// - Метод hasOwnProperty можно использовать для определения собственное свойство или нет
+
+// x собстветтное свойство objB поэтому true
+console.log(objB.hasOwnProperty("x")); // log true
+
+// z унаследованное свойство, поэтому false
+console.log(objB.hasOwnProperty("z")); // log false
+
+// цикл for...in перебирает не только собствнные, но и унаследованные свойства по всей цепочке прототипов
+// это никогда не нужно, поэтому его не юзаем
+for (const key in objB) {
+  console.log(key); // log x и y и q и z
+}
+// можно использовать метод hasOwnProperty
+// это предикатная функция которая возвращает true или false
+// но лучше всего использовать Object.keys
+console.log(Object.keys(objB)); // (2) ["x", "y"]
+
+*/
+
+/*
+ *
+ * Пишем класс Hero
+ * Статические свойства и методы
+ *
+ */
+
+/*
+
+const Hero = function (name, xp) {
+  this.name = name;
+  this.xp = xp;
+};
+
+// добавляем новое свойство в Hero.prototype
+Hero.prototype.changeName = function (name) {
+  this.name = name;
+};
+
+const mango = new Hero("Mango", 1000);
+
+// экземпляр класса Hero - это объект, и в __proto__ лежит ссылка на Hero.prototype
+console.log(mango); // log Hero {name: "Mango", xp: 1000}
+
+// в свойстве Hero.prototype мы будем хранить методы на нашего героя (прототипное наследование)
+console.log(Hero.prototype); // log {changeName: ƒ, constructor: ƒ}
+
+// Статические свойства и методы
+// они не доступны на экземплярах
+// Они есть только на самом конструкторе
+
+// .pow это статический метод
+console.log(Math.pow(2, 5));
+
+// .PI это статическое свойство
+console.log(Math.PI);
+
+// .parseInt это статический метод
+console.log(Number.parseInt("5a"));
+
+Hero.description = "gggkuihiuihvvg;ugufurtdu";
+
+Hero.showStats = function (hero) {
+  console.log(hero);
+};
+
+console.dir(Hero);
+
+// пример статитециских методов
+// это методы конструкторов
+// Array.from();
+// Number.parseInt();
+// Object.keys();
+
+*/
+
+/*
+ *
+ * Класс Hero
+ * Статические свойства и методы
+ * - Синтаксический сахар
+ *
+ */
+
+/*
+
+// class - зарезервированное слово
+// Hero - имя класса
+// {} - открываем тело класса (в теле используются свои правила синтаксиса)
+// методы класса записываются в его свойство prototyp
+
+class Hero {
+  constructor(name, xp) {
+    this.name = name;
+    this.xp = xp;
+  }
+
+  changeName(name) {
+    this.name = name;
+  }
+
+  // добавить опыта герою
+  gainXp(amount) {
+    console.log(`${this.name} получил ${amount} опыта`);
+    this.xp += amount;
+  }
+}
+
+const mango = new Hero("Mango", 1000);
+
+console.dir(Hero); // log class Hero
+console.dir(mango); // log class Hero
+
+// добавляем опыт герою
+mango.gainXp(2000);
+console.log(mango); // log Hero {name: "Mango", xp: 3000}
+
+*/
+/*
+
+// Статические свойства
+// в статических методах вообще нету this
+
+class Hero {
+  // это новый синтаксис статических свойств
+  static description = "labsivaivavaiuv";
+
+  // это новый синтаксис статических метадов
+  static showStats(hero) {
+    // в статических методах this это будет ссылка на сам класс
+    console.log("this:", this);
+  }
+
+  constructor(name, xp) {
+    this.name = name;
+    this.xp = xp;
+  }
+
+  changeName(name) {
+    this.name = name;
+  }
+
+  gainXp(amount) {
+    console.log(`${this.name} получил ${amount} опыта`);
+    this.xp += amount;
+  }
+}
+
+console.dir(Hero);
+
+const mango = new Hero("Mango", 1000);
+
+mango.gainXp(2000);
+console.log(mango);
+
+Hero.showStats(mango);
+
+*/
+// /*
+
+// Геттеры и Сеттеры
+// имя свойства и имя геттера должны быть разные иначе все зациклится
+// по-этому перед именем свойства ставят подчеркивание _name
+// пара get и set должна называться одинаково
+
+class Hero {
+  static description = "labsivaivavaiuv";
+
+  static showStats(hero) {
+    console.log("this:", this);
+  }
+
+  constructor(name, xp) {
+    this._name = name;
+    this.xp = xp;
+  }
+
+  // геттер
+  get name() {
+    return this._name;
+  }
+
+  // сеттер
+  set name(newName) {
+    this._name = newName;
+  }
+
+  // Old School
+  // changeName(name) {
+  //   this.name = name;
+  // }
+
+  // getName() {
+  //   return this.name;
+  // }
+
+  gainXp(amount) {
+    console.log(`${this.name} получил ${amount} опыта`);
+    this.xp += amount;
+  }
+}
+
+console.dir(Hero);
+
+const mango = new Hero("Mango", 1000);
+
+mango.name = "Mangocooldog";
+console.log(mango.name); // log Mangocooldog
+
+// mango.gainXp(2000);
+// console.log(mango);
+
+// Hero.showStats(mango);
