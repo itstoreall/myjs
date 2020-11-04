54dbbd6658473bec6cbf119a6250ad3d9c9b3fb7
Serhii Stanislav <serhiistanislav@gmail.com>
2020-11-04 17:17:51 +0200

    adds 007 Highlights (part)

--- js/007-highlights.js
+++ js/007-highlights.js
@@ -1,15 +1,35 @@
 
 /* =========================================== 
 
-linkRef
+Найти и показать
 
-- Поиск элемента по классу
+- Найти элемент по классу
 const linkRef = document.querySelector(".nav__link");
 * <a href="" class="nav__link">link</a>
 
-- Поиск всех элементов по классу
+- Найти все элементы по классу
 const linkRef = document.querySelectorAll(".nav__link");
 * NodeList(2)[a.nav__link, a.nav__link.nav__link--active]
+
+- Найти по id
+const linkRef = document.querySelector("#nav__link");
+* <a href="" id="nav__link">link</a>
+
+- Найти кнопку по data-*
+const incBtnRef = document.querySelector('button[data-action="increment"]');
+* <button type="button" data-action="increment">+1</button>
+
+- Показать текст в элементе
+console.log(element.textContent);
+
+------------------------------------------- */
+
+/* ===========================================
+
+Присвоение и замена
+
+- Изменяем текст в элементе
+element.textContent = 'Новый текст';
 
 ------------------------------------------- */
 
@@ -20,7 +40,7 @@ titleRef
 const titleRef = document.querySelector("h3");
 * <h3>Highlights</h3>
 
-- Создаем класс заголовка
+- Создаем класс заголовка (oldS)
 titleRef.setAttribute("class", "page-h3")
 
 - Изменяем текст заголовка
@@ -182,7 +202,11 @@ const createProductCard = product => {
    - h3
    const titleRef3 = document.createElement("h3");
    titleRef3.textContent = product.name;
-   titleRef3.classList.add("product-card__name");
+
+   - Добавляем класс через терн. опер. (меняет цвет before)
+   titleRef3.classList.add("product-card__name", product.available 
+   ? "product-card__name--available" 
+   : "product-card__name--not-available");
 
    - description
    const descrRef = document.createElement("p");
@@ -204,5 +228,96 @@ const productCards = products.map(product => createProductCard(product));
 - Распыляем массив карточек в DOM
 const productsListRef = document.querySelector(".js-products");
 productsListRef.append(...productCards);
+
+------------------------------------------- */
+
+/* ===========================================
+
+ul.categories > li.item > h2, ul > li
+
+- Достучаться ко всем li.item
+const items = document.querySelectorAll(".item");
+* NodeList(3) [li.item, li.item, li.item]
+
+- Узнать длину items (псевдо-мас)
+const quantityItems = items.length;
+
+- Перебрать items в forEach
+items.forEach(item => {
+
+   - Показать текст из h2
+   item.children[0].innerText;
+
+   - Показать количество li в ul
+   item.children[1].children.length;
+})
+
+------------------------------------------- */
+
+/* ===========================================
+
+ul.ingredients
+
+- Имеем массив ингредиентов
+const ingredients = ['Картошка', 'Грибы', 'Чеснок'];
+
+- Находим ul.ingredients
+const ingredientsRef = document.querySelector(".ingredients");
+
+- Перебираем массив ингредиентов
+ingredients.forEach(element => {
+
+   - Создаем li
+   const ingredient = document.createElement("li");
+
+   - Добавляем в li текс из массива
+   ingredient.textContent = element;
+
+   - Впихиваем li в ul
+   ingredientsRef.appendChild(ingredient);
+})
+
+------------------------------------------- */
+
+/* ===========================================
+
+Counter (increment, decrement)
+
+- Имеем такую конструкцию в HTML
+<div id="counter">
+   <button type="button" data-action="decrement">-1</button>
+   <span id="value">0</span>
+   <button type="button" data-action="increment">+1</button>
+</div>
+
+- Находим кнопку increment по data=*
+const incrementBtnRef = document.querySelector('button[data-action="increment"]');
+
+- Находим кнопку decrement по data=*
+const decrementBtnRef = document.querySelector('button[data-action="decrement"]');
+
+- Находим span (отображение счетчика)
+const counterValueRef = document.querySelector("#value");
+
+- Создаем переменную значения счетчика
+let counterValue = 0;
+
+- Создаем функцию increment (+1)
+incrementBtnRef.addEventListener("click", () => {
+   counterValue += 1;
+
+   - Записывает значение в переменную
+   counterValueRef.textContent = counterValue;
+});
+
+- Создаем функцию decrement (11)
+decrementBtnRef.addEventListener("click", () => {
+   if (counterValue < 1) {
+      counterValue = 0;
+   } else { counterValue -= 1 };
+
+   - Записывает значение в переменную
+   counterValueRef.textContent = counterValue;
+});
 
 ------------------------------------------- */
