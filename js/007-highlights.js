66a64409d088ca5ebb2d6db78c01775868ec559d
Serhii Stanislav <serhiistanislav@gmail.com>
2020-11-08 01:25:47 +0200

    adds 007 highlights

--- js/007-highlights.js
+++ js/007-highlights.js
@@ -1,7 +1,6 @@
 
 /* =========================================== 
-
-Найти и показать
+** Найти и показать
 
 - Найти элемент по классу
 const linkRef = document.querySelector(".nav__link");
@@ -25,32 +24,75 @@ console.log(element.textContent);
 ------------------------------------------- */
 
 /* ===========================================
+** Создание, присвоение, добавление
 
-Присвоение и замена
-
-- Изменяем текст в элементе
-element.textContent = 'Новый текст';
-
-------------------------------------------- */
-
-/* ===========================================
-
-titleRef
-
-const titleRef = document.querySelector("h3");
-* <h3>Highlights</h3>
+- Создаем div
+const containerRef = document.createElement("div");
 
 - Создаем класс заголовка (oldS)
 titleRef.setAttribute("class", "page-h3")
 
-- Изменяем текст заголовка
-titleRef.textContent = "Новый Highlights";
+- Изменяем текст в элементе
+element.textContent = 'Новый текст';
+
+- Присваивает спану значение введенное в инпут
+nameNameOutput.textContent = event.target.value;
+
+- Присваиваем значение data-length
+const inpunDataLength = inputRef.dataset.length;
+
+- Добавляем и удаляем класс
+inputRef.classList.remove("invalid");
+inputRef.classList.add("valid");
+
+- Добавляем атрибут для input type="range"
+input.setAttribute("min", "16");
+
+- Добавляем спану инлайн-стиль font-size с текущим значением инпута
+span.style.fontSize = event.currentTarget.value + "px";
+
+- Увеличиваем на 10 начальное значение (из инпута) amount раз 
+const initialSize = 30;
+for (let i = 0; i < amount; i += 1) {
+   const size = initialSize + i * 10;
+};
+
+- Функция берет значение из инпута (type="number")
+function getInputValue() {
+  const inputValue = document.querySelector("#controls > input").value;
+
+  - и вызываем функцию создания дива
+  createBoxes(inputValue);
+};
+
+- Добавляем диву инлайн-стили: ширина, высота, рандомный цвет
+div.style.cssText = `width: ${size}px; height: ${size}px; background-color: rgba( ${randomColor()} , ${randomColor()} , ${randomColor()} )`;
+
+- Функция создания рандомного цвета
+function randomColor() {
+  return Math.floor(Math.random() * 256);
+};
+
+- Функция очистки инпута
+function destroyBoxes() {
+  boxesRef.textContent = "";
+};
 
 ------------------------------------------- */
 
 /* ===========================================
+** if else
 
-activeLinkRef
+- Если значение спана пустая строка
+if (nameNameOutput.textContent === "")
+
+- Если кол-во введенных в input = значению data-length
+if (inputValidationRef.value.length === Number(inpunDataLength))
+
+------------------------------------------- */
+
+/* ===========================================
+** activeLinkRef
 
 const activeLinkRef = document.querySelector(".nav__link--active");
 * <a class="nav__link nav__link--active">link Active</a>
@@ -70,8 +112,7 @@ activeLinkRef.classList.add("new-class-activeLink");
 ------------------------------------------- */
 
 /* ===========================================
-
-imageRef
+** imageRef
 
 const imageRef = document.querySelector(".hero-img");
 * <img class="test-img" src="./images/img-001.jpg">
@@ -91,8 +132,7 @@ imageRef.removeAttribute("alt");
 ------------------------------------------- */
 
 /* ===========================================
-
-classList
+** classList
 
 const linkRef = document.querySelector(".nav__link");
 console.log(linkRef.classList)
@@ -125,8 +165,7 @@ linkRef.classList.forEach(cls => {
 ------------------------------------------- */
 
 /* ===========================================
-
-createElement
+** createElement
 
 - Создаем элемент h3 в пямяти
 const titleRef2 = document.createElement("h3");
@@ -143,8 +182,9 @@ titleRef2.id = "created__title-id";
 ------------------------------------------- */
 
 /* ===========================================
+** createElement
 
-createElement
+* Создаем карточку 
 
 - Создаем div в пямяти
 const containerRef = document.createElement("div");
@@ -186,8 +226,9 @@ cardRootRef.appendChild(containerRef)
 ------------------------------------------- */
 
 /* ===========================================
+** product => {}
 
-product => {}
+* Создаем крточку с помощью функции
 
 - Импортируем объект с продуктами
 import products from "./007-products.js";
@@ -232,8 +273,9 @@ productsListRef.append(...productCards);
 ------------------------------------------- */
 
 /* ===========================================
+** ul.categories > li.item > h2, ul > li
 
-ul.categories > li.item > h2, ul > li
+* Cчитаем кол-во li разной глубины и выводим текст из h2
 
 - Достучаться ко всем li.item
 const items = document.querySelectorAll(".item");
@@ -255,8 +297,9 @@ items.forEach(item => {
 ------------------------------------------- */
 
 /* ===========================================
+** ul.ingredients
 
-ul.ingredients
+* Создаем li из массива и впихиваем их в ul
 
 - Имеем массив ингредиентов
 const ingredients = ['Картошка', 'Грибы', 'Чеснок'];
@@ -280,8 +323,9 @@ ingredients.forEach(element => {
 ------------------------------------------- */
 
 /* ===========================================
+** Counter
 
-Counter (increment, decrement)
+* Кнопкими increment и decrement изменяем значение счетчика
 
 - Имеем такую конструкцию в HTML
 <div id="counter">
@@ -319,5 +363,148 @@ decrementBtnRef.addEventListener("click", () => {
    - Записывает значение в переменную
    counterValueRef.textContent = counterValue;
 });
+
+------------------------------------------- */
+
+/* ===========================================
+** Input =-> h2 > text
+
+* Когда вводим текст в input он отображается в h2
+
+- Имеем два тега
+<input type="text" placeholder="Ваше имя?" id="name-input" />
+<h1>Привет, <span id="name-output">незнакомец</span>!</h1>
+
+- Находим итпут и спан
+const nameInputRef = document.querySelector("#name-input");
+const nameNameOutput = document.querySelector("#name-output");
+
+- Функция отображения текста в заголовке
+nameInputRef.addEventListener("input", event => {
+
+   - Присваиваем спану значение вписанное в инпут
+   nameNameOutput.textContent = event.target.value;
+   
+   - Если спан пустой, будет отображаться "незнакомец"
+   if (nameNameOutput.textContent === "") {
+      nameNameOutput.textContent = "незнакомец";
+   };
+});
+
+------------------------------------------- */
+
+/* ===========================================
+** Input data-length blur =-> classList
+
+* При 6 символах бордер зеленый, иначе - красный
+
+- Имеем input с data-length
+<input type="text" id="validation-input" data-length="6" placeholder="Введи 6 символов" />
+
+- Находим input по id
+const inputValidationRef = document.querySelector("#validation-input");
+
+- Присваиваем значение data-length
+const inpunDataLength = inputValidationRef.dataset.length;
+
+- Функция - при потере фокуса меняем классы
+inputValidationRef.addEventListener("blur", () => {
+
+   - Если кол-во символов = data-length ...
+   if (inputValidationRef.value.length === Number(inpunDataLength)) {
+      inputValidationRef.classList.remove("invalid");
+      inputValidationRef.classList.add("valid");
+   } else if (inputValidationRef.value.length === 0) {
+      inputValidationRef.classList.remove("valid");
+      inputValidationRef.classList.remove("invalid");
+   } else {
+      inputValidationRef.classList.remove("valid");
+      inputValidationRef.classList.add("invalid");
+   }
+});
+
+------------------------------------------- */
+
+/* ===========================================
+** input type="range" =-> fontSize
+
+- Имеем input и span
+<input id="font-size-control" type="range" />
+<br />
+<span id="text">Абракадабра!</span>
+
+- Находим input
+const input = document.querySelector("#font-size-control");
+
+- Находим span
+const span = document.querySelector("#text");
+
+- Добавляем атрибуты для input type="range"
+input.setAttribute("min", "16")
+input.setAttribute("max", "45")
+
+- Вешаем слушатель на input
+input.addEventListener("input", onInputRange);
+
+- Добавляем онлайн-стиль font-size (fontSize) спану 
+function onInputRange(event) {
+
+   - который изменяется от текущего значения inputa range
+  span.style.fontSize = event.currentTarget.value + "px";
+};
+
+------------------------------------------- */
+
+/* ===========================================
+** input value =-> div.style.cssText
+
+// - Находим div и кнопку по data-*
+
+const renderBtnRef = document.querySelector('button[data-action="render"]');
+const destroyBtnRef = document.querySelector('button[data-action="destroy"]');
+const boxesRef = document.getElementById("boxes");
+
+// - Запускаем кликом функцию получения значения из инпута (type="number")
+// и функцию очистки строки (удалить контент в input)
+
+renderBtnRef.addEventListener("click", getInputValue);
+destroyBtnRef.addEventListener("click", destroyBoxes);
+
+// 2. - Функция создает div в который впихиваются маленькие дивы 
+// с начальным значением созданные в цикле, рандомных цветов 
+// и динамическим размером. Количество итераций определяется  
+// amount - значением инпута type="number"
+
+function createBoxes(amount) {
+  const initialSize = 30;
+  const containerRef = document.createElement("div");
+  for (let i = 0; i < amount; i += 1) {
+    const size = initialSize + i * 10;
+    const newDiv = document.createElement("div");
+    newDiv.style.cssText = `width: ${size}px; height: ${size}px; background-color: rgba( ${randomColor()} , ${randomColor()} , ${randomColor()} )`;
+    containerRef.appendChild(newDiv);
+  }
+  boxesRef.appendChild(containerRef);
+};
+
+// 1. - Функция берет из инпута значение, и в его 
+// контексте запускает создание дивчика 
+
+function getInputValue() {
+  const inputValue = document.querySelector("#controls > input").value;
+  createBoxes(inputValue);
+};
+
+// 3. - Функция создает рандомные цвета rgba
+
+function randomColor() {
+  return Math.floor(Math.random() * 256);
+};
+
+// 4. - Функция очищает строку (инпут)
+
+function destroyBoxes() {
+  boxesRef.textContent = "";
+};
 
 ------------------------------------------- */
